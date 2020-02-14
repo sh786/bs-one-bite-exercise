733ecb9df1a8c7ae8d5623fbbe2ebab647cc7305
Sam Hamburger <samhamburger@Sams-MBP-2.lan1>
2020-02-14 16:18:33 -0500

    Updates

---
+++ precache-manifest.2bbb0ad56fb9e4736f885d79225829b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f0d7aba70d3e45edd7e2adeabc81d7",
+    "url": "/bs-one-bite-exercise/index.html"
+  },
+  {
+    "revision": "9efe95446ae3146d969b",
+    "url": "/bs-one-bite-exercise/static/css/main.54390dc3.chunk.css"
+  },
+  {
+    "revision": "9189eabad614e5fbe76d",
+    "url": "/bs-one-bite-exercise/static/js/2.ff06d784.chunk.js"
+  },
+  {
+    "revision": "9efe95446ae3146d969b",
+    "url": "/bs-one-bite-exercise/static/js/main.30bf2409.chunk.js"
+  },
+  {
+    "revision": "9cc3c7d5c1fd568f93e9",
+    "url": "/bs-one-bite-exercise/static/js/runtime~main.1c1990f9.js"
+  }
+]);
